ffaeb7fc7642ef31e9da327e78129a8ed3bb8bb3
garnetddolphin <ddolphin.loves.gc@gmail.com>
2022-06-24 08:29:30 +0900

    Display draw message

--- src/index.js
+++ src/index.js
@@ -123,7 +123,11 @@ class Game extends React.Component {
 		if (winner) {
 			status = "Winner: " + winner;
 		} else {
-			status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+			if (winInfo.isDraw) {
+				status = "Draw";
+			} else {
+				status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+			}
 		}
 
 		if(!isAscending) {
@@ -173,10 +177,21 @@ function calculateWinner(squares){
 			return {
 				winner: squares[a],
 				line: lines[i],
+				isDraw: false,
 			};
+		}
+	}
+
+	let isDraw = true;
+	for (let i = 0; i < squares.length; i++) {
+		if (squares[i] === null) {
+			isDraw = false;
+			break;
 		}
 	}
 	return {
 		winner: null,
+		line: null,
+		isDraw: isDraw,
 	};
 }
